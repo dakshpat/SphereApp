0de0eefd4ab3281b0088505d17965f37bfb14816
Daksh <dpatel132@hawk.iit.edu>
2022-01-21 13:09:46 -0600

    sphere-bad

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -9,9 +9,20 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
+        Button("Calculate", action: {let _ = self.sphereVolume()})
+            .padding(.bottom)
             .padding()
     }
+    
+
+    func sphereVolume() -> Double{
+        let sphereRadius = 0.0
+        let volume = 4/3*Double.pi*sphereRadius
+        print(volume)
+        return volume
+    }
+
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
@@ -19,3 +30,4 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
---
+++ Shared/Sphere.swift
@@ -0,0 +1,13 @@
+//
+//  Sphere.swift
+//  SphereApp (iOS)
+//
+//  Created by Daksh Patel on 1/21/22.
+//
+
+import Foundation
+import SwiftUI
+
+    
+
+
