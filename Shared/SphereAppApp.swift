fc8eca00800644f3beed0a1c1ed514594a804de7
Daksh <dpatel132@hawk.iit.edu>
2022-01-21 12:22:40 -0600

    Initial Commit

---
+++ Shared/SphereAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SphereAppApp.swift
+//  Shared
+//
+//  Created by Daksh Patel on 1/21/22.
+//
+
+import SwiftUI
+
+@main
+struct SphereAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
