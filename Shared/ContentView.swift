4c435b0bb188ef2f7805ba1280c50fb4de146734
Daksh <dpatel132@hawk.iit.edu>
2022-01-21 17:37:48 -0600

    Example 2: working model of user input

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -8,28 +8,43 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+   @State var sphereRadius = "0.0"
+   @State var sphereVolume = "0.0"
+    
     var body: some View {
-        Button("Calculate", action: {let _ = self.sphere()})
+        
+        VStack{
+            Text("Sphere Radius")
+                .padding(.top)
+                .padding(.bottom, 0)
+            TextField("Enter Radius", text: $sphereRadius, onCommit: {self.sphere(sphereRadius: Double(sphereRadius)!)})
+                .padding()
+            HStack{
+                Text("Sphere Volume")
+                Text(self.sphere(sphereRadius: Double(sphereRadius)!))
+            }
+                    
+        }
+        
+        
+        Button("Calculate", action: {let _ = self.sphere(sphereRadius: Double(sphereRadius)!)})
             .padding(.bottom)
             .padding()
+    
     }
     
-
-    func sphere() -> Double{
-        let sphereRadius = 5.0
-        let sphereVolume = 4/3*Double.pi*pow(sphereRadius,3)
-        let sphereSurfaceArea = 4*Double.pi*pow(sphereRadius,2)
-        let boxVolume = pow(sphereRadius*2,3)
-        let boxSurfaceArea = pow(sphereRadius*2,2)*6
-        print("sphereVolume: " , sphereVolume)
-        print("sphereSurfaceArea: " , sphereSurfaceArea)
-        print("boxVolume: " , boxVolume)
-        print("boxSurfaceArea: " , boxSurfaceArea)
-        return sphereVolume
-    }
-
     
+    
+    func sphere(sphereRadius: Double) -> Double{
+        let sphereVol = 4/3*Double.pi*pow(sphereRadius,3)
+        print("Sphere Volume: " , sphereVol)
+        return sphereVol
+    }
 }
+    
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
