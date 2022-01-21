26d46da95263c5b41f8a5829cf0a8be0762c181e
Daksh <dpatel132@hawk.iit.edu>
2022-01-21 13:24:43 -0600

    First example: writing the code poorly

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -9,17 +9,23 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Button("Calculate", action: {let _ = self.sphereVolume()})
+        Button("Calculate", action: {let _ = self.sphere()})
             .padding(.bottom)
             .padding()
     }
     
 
-    func sphereVolume() -> Double{
+    func sphere() -> Double{
         let sphereRadius = 5.0
-        let volume = 4/3*Double.pi*pow(sphereRadius,3)
-        print(volume)
-        return volume
+        let sphereVolume = 4/3*Double.pi*pow(sphereRadius,3)
+        let sphereSurfaceArea = 4*Double.pi*pow(sphereRadius,2)
+        let boxVolume = pow(sphereRadius*2,3)
+        let boxSurfaceArea = pow(sphereRadius*2,2)*6
+        print("sphereVolume: " , sphereVolume)
+        print("sphereSurfaceArea: " , sphereSurfaceArea)
+        print("boxVolume: " , boxVolume)
+        print("boxSurfaceArea: " , boxSurfaceArea)
+        return sphereVolume
     }
 
     
