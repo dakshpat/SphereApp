ec76aead12c7e3887b0390a3d0965529eff94cc9
Daksh <dpatel132@hawk.iit.edu>
2022-01-23 14:56:23 -0600

    Second Example complete

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -9,8 +9,11 @@ import SwiftUI
 
 struct ContentView: View {
     
-   @State var sphereRadius = "0.0"
-   @State var sphereVolume = "0.0"
+    @State var sphereRadius = 0.0
+    @State var sphereVolume = 0.0
+    @State var sphereSurfaceArea = 0.0
+    @State var boxVolume = 0.0
+    @State var boxSurfaceArea = 0.0
     
     var body: some View {
         
@@ -18,28 +21,31 @@ struct ContentView: View {
             Text("Sphere Radius")
                 .padding(.top)
                 .padding(.bottom, 0)
-            TextField("Enter Radius", text: $sphereRadius, onCommit: {self.sphere(sphereRadius: Double(sphereRadius)!)})
-                .padding()
-            HStack{
-                Text("Sphere Volume")
-                Text(self.sphere(sphereRadius: Double(sphereRadius)!))
-            }
-                    
+    
+            TextField("Enter Radius", value: $sphereRadius, format: .number)
+    
+            Text("Sphere Volume: \(sphereVolume)")
+                .padding(0.5)
+            Text("Sphere SurfaceArea: \(sphereSurfaceArea)")
+                .padding(0.5)
+            Text("Bounding Box Volume: \(boxVolume)")
+                .padding(0.5)
+            Text("Bounding Box SurfaceArea: \(boxSurfaceArea)")
+            .padding(0.5)
+            
         }
         
         
-        Button("Calculate", action: {let _ = self.sphere(sphereRadius: Double(sphereRadius)!)})
-            .padding(.bottom)
+        Button("Calculate", action:{
+            sphereVolume = (4/3)*Double.pi*pow(sphereRadius,3)
+            sphereSurfaceArea = 4*Double.pi*pow(sphereRadius,2)
+            boxSurfaceArea = 6*pow(2*sphereRadius,2)
+            boxVolume = pow(2*sphereRadius,3)
+            
+        })
             .padding()
+        
     
-    }
-    
-    
-    
-    func sphere(sphereRadius: Double) -> Double{
-        let sphereVol = 4/3*Double.pi*pow(sphereRadius,3)
-        print("Sphere Volume: " , sphereVol)
-        return sphereVol
     }
 }
     
